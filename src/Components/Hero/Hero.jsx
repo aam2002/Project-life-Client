6fc37fa32e4ba367f2ba12a3f86d288798c6e909
julikray <julik.roy@gmail.com>
2023-10-24 10:21:01 +0530

    hero-section done

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -22,38 +22,6 @@ function Hero() {
   }, []);
   return (
     <>
-      {/* <div className="homemain">
-        <div className="homebox">
-          <div className="flex md:w-[80vw] justify-between mx-auto text-img md:flex-row flex-col-reverse w-[100vw] gap-24">
-            <div className="left-box flex align-middle justify-center">
-              <div className="flex flex-column align-middle justify-center">
-                <h2 className="bhagvat-heading">Bhagvat Gita:</h2>
-                <p className="H2-tag">
-                  Your path to <br /> divinity{" "}
-                </p>
-
-                <p className="para-text"> Read and Discover Inner Peace</p>
-                <div className="scroll-button">
-                  <span>Scroll to Read</span>{" "}
-                  <FontAwesomeIcon icon={faCaretDown} />
-                </div>
-              </div>
-            </div>
-            <div className="right-box">
-              <div className="cover-img ">
-                <img src="cover-copy-2.png" alt="cover" />
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="w-[100px] mx-auto dots-container">
-          <Dots rows={10} columns={5} />
-        </div>
-      </div> */}
-
-
-
-      {/* JKroy code change here */}
       <div className="homemain">
         <div className="homeboxdiv">
           <div className="homebox">
@@ -73,34 +41,16 @@ function Hero() {
             </div>
           </div>
 
-
           <div className="dots-container">
             <Dots rows={8} columns={5} />
           </div>
         </div>
-
-
-
-
       </div>
-
-
-
-      <div className="homemain2">  
-      <div className="dots-container2">
-        <Dots rows={2} columns={5} />
-      </div></div>
-
-
-
-
-
-
-
-
-
-
-
+      <div className="homemain2">
+        <div className="dots-container2">
+          <Dots rows={2} columns={5} />
+        </div>
+      </div>
 
     </>
   );
