ad90bce4fe7ab811ea09b2c0ee46220a49d8f899
aman <amansoni20020@gmail.com>
2023-10-27 23:05:41 +0530

    fixed the hero section

--- src/Components/Our-purpose/OurPurpose.jsx
+++ src/Components/Our-purpose/OurPurpose.jsx
@@ -6,45 +6,40 @@ function OurPurpose() {
   const { data } = useContext(DataContext);
   console.log(data);
   return (
-    <div className="purpose">
-      <div className="purpose-text">
-        <p className="purpose-heading">Our Purpose</p>
-        <p className="purpose-para">Our Purpose Behind This Website</p>
-      </div>
-      <div className="purpose-box">
-        <div className="p-p">
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio, minus
-            quos, beatae deleniti ut delectus consequuntur, pariatur quae
-            nostrum est aliquam tenetur quisquam repudiandae. Omnis iure illo
-            dolorum exercitationem possimus? Lorem ipsum dolor sit amet
-            consectetur adipisicing elit. Odio, minus quos, beatae deleniti ut
-            delectus consequuntur, pariatur quae nostrum est aliquam tenetur
-            quisquam repudiandae. Omnis iure illo dolorum exercitationem
-            possimus? Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Odio, minus quos, beatae deleniti ut.
-            <br />
-            <br />
-            
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio,
-              minus quos, beatae deleniti ut delectus consequuntur, pariatur
-              quae nostrum est aliquam tenetur quisquam repudiandae. Omnis iure
-              illo dolorum exercitationem possimus? Lorem ipsum dolor sit amet
-              consectetur adipisicing elit. Odio, minus quos, beatae deleniti ut
-              delectus consequuntur, pariatur quae nostrum est aliquam tenetur
-              quisquam repudiandae. Omnis iure illo dolorum exercitationem
-              possimus? Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Odio, minus quos, beatae deleniti ut.
-           
-          </p>
+    <div className="our_purpose">
+      <div className="purpose">
+        <div className="purpose-text">
+          <p className="purpose-heading">Our Purpose</p>
+          <p className="purpose-para">Our Purpose Behind This Website</p>
         </div>
-        <div className="purpose-img">
-          <img src="logoblack.png" alt="" />
+        <div className="purpose-bigbox">
+          <div className="purpose-box">
+            <div className="p-p">
+              <p>
+                The purpose of a Bhagavad Gita website is to serve as an online platform dedicated to the teachings, wisdom, and philosophy of the Bhagavad Gita, a sacred Hindu scripture.
+                <br />
+                This website aims to fulfill several important objectives:
+                <br />
+                <b>Education and Awareness:</b> The primary purpose is to educate people about the Bhagavad Gita's profound teachings. It provides accessible information about the scripture's context, content, and significance, making it available to a global audience.
+                <br />
+                <b>Spiritual Guidance:</b> The website offers guidance on how individuals can apply the Gita's teachings in their daily lives to attain spiritual growth, inner peace, and a sense of purpose.
+                <br />
+                <b>Cultural Preservation:</b> It plays a role in preserving and promoting the cultural and philosophical heritage of India, ensuring that the wisdom of the Gita is passed down to future generations.
+                <br />
+                <b>Inspiration and Motivation:</b> The website can inspire and motivate individuals to lead a purposeful life, emphasizing concepts like duty, righteousness, and self-realization.
+
+              </p>
+            </div>
+            <div className="purpose-circle">
+              <Dots rows={5} columns={15} />
+            </div>
+          </div>
+          <div className="purpose-img">
+            <img src="logoblack.png" alt="" />
+          </div>
         </div>
       </div>
-      <div className="purpose-circle">
-        <Dots rows={5} columns={15} />
-      </div>
+
     </div>
   );
 }
