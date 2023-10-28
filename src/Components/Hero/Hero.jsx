2e4812c3f28eb93039dd42c033c440b32525c6ff
Aman <amansoni20020@gmail.com>
2023-10-28 11:51:09 +0530

    pulling for master

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -23,34 +23,6 @@ function Hero() {
   }, []);
   return (
     <>
-      {/* <div className="homemain">
-        <div className="homebox">
-          <div className="flex md:w-[80vw] justify-between mx-auto text-img md:flex-row flex-col-reverse w-[100vw] gap-24">
-            <div className="left-box flex align-middle justify-center">
-              <div className="flex flex-column align-middle justify-center">
-                <h2 className="bhagvat-heading">Bhagvat Gita:</h2>
-                <p className="H2-tag">
-                  Your path to <br /> divinity{" "}
-                </p>
-
-                <p className="para-text"> Read and Discover Inner Peace</p>
-                <div className="scroll-button">
-                  <span>Scroll to Read</span>{" "}
-                  <FontAwesomeIcon icon={faCaretDown} />
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="right-box">
-          <div className="cover-img ">
-            <img src="cover-copy-2.png" alt="cover" />
-          </div>
-        </div>
-        <div className="w-[100px] mx-auto dots-container">
-          <Dots rows={10} columns={5} />
-        </div>
-      </div> */}
       <div className="main">
 
       <div className="homelanding w-[100vw]">
