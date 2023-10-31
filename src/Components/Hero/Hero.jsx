e09f575e5b2aa4581754c40e9af24a61fae98952
julikray <julik.roy@gmail.com>
2023-10-31 08:43:01 +0530

    update

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -23,30 +23,6 @@ function Hero() {
   }, []);
   return (
     <>
-      <div className="homemain">
-        <div className="homeboxdiv">
-          <div className="homebox">
-            <div className="homeboxheading">
-              <h2 className="bhagvat-heading">Bhagvat Gita:</h2>
-              <p className="H2-tag">
-                Your path to <br /> divinity{" "}
-              </p>
-              <p className="para-text"> Read and Discover Inner Peace</p>
-              <div className="scroll-button">
-                <span>Scroll to Read</span>{" "}
-                <FontAwesomeIcon icon={faCaretDown} />
-              </div>
-            </div>
-            <div className="right-box">
-              <img src="cover-copy-2.png" alt="cover" />
-            </div>
-          </div>
-
-          <div className="dots-container">
-            <Dots rows={8} columns={5} />
-          </div>
-        </div>
-      </div> 
       <div className="main">
 
       <div className="homelanding w-[100vw]">
@@ -77,12 +53,6 @@ function Hero() {
         </div>
       </div>
       </div>
-      <div className="homemain2">
-        <div className="dots-container2">
-          <Dots rows={2} columns={5} />
-        </div>
-      </div>
-
     </>
   );
 }
