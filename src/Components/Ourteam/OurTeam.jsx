ed6bc79268911c83cc6909a09ecdadc30fb35f37
Aman <amansoni20020@gmail.com>
2023-11-02 10:43:30 +0530

    OurTeam got responsive

--- src/Components/Ourteam/OurTeam.jsx
+++ src/Components/Ourteam/OurTeam.jsx
@@ -11,6 +11,10 @@ import "./OurTeam.css";
 const Example = () => {
   return (
     <div>
+      {/* <div className="w-[80vw] flex mx-auto mt-20 flex-col">
+        <p className="content-h">Our Team</p>
+        <p className="content-p">Meet Our Team Members</p>
+      </div> */}
       <HorizontalScrollCarousel />
     </div>
   );
@@ -22,13 +26,16 @@ const HorizontalScrollCarousel = () => {
     target: targetRef,
   });
 
-  const x = useTransform(scrollYProgress, [0, 1], ["1%", "-95%"]);
+  const x = useTransform(scrollYProgress, [0, 1], ["43.9%", "-43.3%"]);
 
   return (
     <section ref={targetRef} className="relative h-[700vh] w-[80vw] mx-auto">
-      <div>Our team</div>
-      <div className="sticky top-32 flex h-screen items-center overflow-hidden">
-        <motion.div style={{ x }} className="flex gap-4 ">
+      <div className="sticky top-0 flex h-screen  items-center overflow-hidden flex-col">
+        <div className="w-[80vw] flex mx-auto mt-24 flex-col">
+          <p className="content-h">Our Team</p>
+          <p className="content-p">Meet Our Team Members</p>
+        </div>
+        <motion.div style={{ x }} className="flex gap-4 motion-div">
           {cards.map((card) => {
             return <Card card={card} key={card.id} />;
           })}
@@ -39,7 +46,7 @@ const HorizontalScrollCarousel = () => {
 };
 
 const Card = ({ card }) => {
-  return (
+  return window.screen.availWidth > 768 ? (
     <div
       key={card.id}
       className="flex items-center justify-center group h-[750px] w-[950px] overflow-hidden mx-auto"
@@ -62,22 +69,22 @@ const Card = ({ card }) => {
               <div className="flex flex-row">
                 <ul className="flex flex-row w-[300px] justify-between my-4">
                   <li>
-                    <a href="">
+                    <a href=".">
                       <img width="40px" src={Discord} alt="" />{" "}
                     </a>
                   </li>
                   <li>
-                    <a href="">
+                    <a href=".">
                       <img width="40px" src={Insta} alt="" />
                     </a>
                   </li>
                   <li>
-                    <a href="">
+                    <a href=".">
                       <img width="40px" src={Facebook} alt="" />
                     </a>
                   </li>
                   <li>
-                    <a href="">
+                    <a href=".">
                       <img width="40px" src={Github} alt="" />
                     </a>
                   </li>
@@ -97,6 +104,60 @@ const Card = ({ card }) => {
                 </button>
               </div>
             </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  ) : (
+    <div
+      key={card.id}
+      className=" about-card-parent flex flex-col items-center justify-center group  overflow-hidden mx-auto"
+    >
+      <div
+        className="about-card-image w-[80%] h-[361px] rounded-[16px] mx-auto "
+        style={{
+          backgroundImage: `url(${mswctj}) `,
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
+      ></div>
+      <div className="flex items-center justify-center">
+        <div className="flex justify-center flex-col items-center mt-3">
+          <p className="about-card-name ">{card.name}</p>
+          <p className="about-card-degination">Frontend Developer</p>
+          <div className="flex flex-col items-center justify-center">
+            <button className="about-card-btn my-2 rounded-lg">
+              {" "}
+              Portfolio
+            </button>
+            <button className="about-card-btn my-2 rounded-lg">Resume</button>
+            <button className="about-card-btn my-2 rounded-lg">Connect</button>
+          </div>
+
+          <div className="flex flex-row">
+            <ul className="flex flex-row w-[70vw] justify-between my-4">
+              <li>
+                <a href=".">
+                  <img width="40px" src={Discord} alt="" />{" "}
+                </a>
+              </li>
+              <li>
+                <a href=".">
+                  <img width="40px" src={Insta} alt="" />
+                </a>
+              </li>
+              <li>
+                <a href=".">
+                  <img width="40px" src={Facebook} alt="" />
+                </a>
+              </li>
+              <li>
+                <a href=".">
+                  <img width="40px" src={Github} alt="" />
+                </a>
+              </li>
+            </ul>
           </div>
         </div>
       </div>
